017b0af66598688c621c7d5dd81c4b66d0d30c08
AKM <akmabrar04@gmail.com>
2025-07-22 20:13:44 +0600

    fs: Import Samsung OEM Changes from OSRC (GKI-Common) X900XXU9DYE3
    Signed-off-by: AKM <akmabrar04@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -64,6 +64,7 @@
 #include <linux/compat.h>
 #include <linux/vmalloc.h>
 #include <linux/io_uring.h>
+#include <linux/task_integrity.h>
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1770,6 +1771,8 @@ static int exec_binprm(struct linux_binprm *bprm)
 		if (depth > 5)
 			return -ELOOP;
 
+		five_bprm_check(bprm, depth);
+
 		ret = search_binary_handler(bprm);
 		if (ret < 0)
 			return ret;
@@ -1830,7 +1833,7 @@ static int bprm_execve(struct linux_binprm *bprm,
 		goto out_unmark;
 
 #ifdef CONFIG_SECURITY_DEFEX
-	retval = task_defex_enforce(current, file, -__NR_execve);
+	retval = task_defex_enforce(current, file, -__NR_execve, bprm);
 	if (retval < 0) {
 		bprm->file = file;
 		retval = -EPERM;
@@ -1855,8 +1858,10 @@ static int bprm_execve(struct linux_binprm *bprm,
 		goto out;
 
 	retval = exec_binprm(bprm);
-	if (retval < 0)
+	if (retval < 0) {
+		task_integrity_delayed_reset(current, CAUSE_EXEC, bprm->file);
 		goto out;
+	}
 
 	/* execve succeeded */
 	current->fs->in_exec = 0;
