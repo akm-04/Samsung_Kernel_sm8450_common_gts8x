f23fbfe50b2f78911d63f7de145a6bf1d25a42cc
AKM <akmabrar04@gmail.com>
2025-07-22 20:13:39 +0600

    drivers: Import Samsung OEM Changes from OSRC (GKI-Common) X900XXU9DYE3
    Signed-off-by: AKM <akmabrar04@gmail.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -77,6 +77,7 @@
 #include <trace/hooks/ipv4.h>
 #include <trace/hooks/pci.h>
 #include <trace/hooks/dmabuf.h>
+#include <trace/hooks/mz.h>
 #include <trace/hooks/wakeupbypass.h>
 #include <trace/hooks/fuse.h>
 
@@ -566,3 +567,4 @@ EXPORT_SYMBOL_GPL(GKI_struct_gic_chip_data);
 #include <linux/swap_slots.h>
 const struct swap_slots_cache *GKI_struct_swap_slots_cache;
 EXPORT_SYMBOL_GPL(GKI_struct_swap_slots_cache);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mz_exit);
