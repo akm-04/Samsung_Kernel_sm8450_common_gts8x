ec0d2c15593717403e0ff5ba39c952b5b8f878d0
AKM <akmabrar04@gmail.com>
2025-08-24 23:37:47 +0600

    mm/zram: add nocfi wrapper and use zram_oem_fn_nocfi to avoid CFI issues
    Introduce a __nocfi wrapper zram_oem_fn_nocfi that forwards to zram_oem_fn and
    replace direct zram_oem_fn calls with the nocfi variant where appropriate.
    
    This fixes kernel panics and bootloop related to zram cfi check failures when booted to system. This patch is related to oem hack that was reverted: refer to cd6a6f6d8426ee4200c98bb767108742d7e410ec
    
    Signed-off-by: AKM <akmabrar04@gmail.com>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -756,6 +756,7 @@ enum zram_oem_func_cmds {
 };
 typedef unsigned long (*zram_oem_func)(int, void *, unsigned long);
 extern zram_oem_func zram_oem_fn;
+extern unsigned long zram_oem_fn_nocfi(int cmd, void *priv, unsigned long param);
 #endif
 #endif /* __KERNEL__*/
 #endif /* _LINUX_SWAP_H */
